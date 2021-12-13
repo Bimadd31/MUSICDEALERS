f313b6c5e93383396c92ce1cbdee937e0c86d6d4
unknown <imadox.paris@hotmail.com>
2021-12-13 22:35:19 +0100

    contact form language change on preference and captcha added

--- ressources/lang_pref.js
+++ ressources/lang_pref.js
@@ -32,14 +32,34 @@
 
 				// ON CLICK ACTION TO CHANGE LANGUAGE
 	function detect_lang_change(){
+
 	if (window.location.hash) {
 		if (window.location.hash === "#FR") {
-			set_fr();
-			visionInfo.textContent = language.fr.visionPage;
+				document.getElementById("captcha_api").setAttribute("src","https://www.google.com/recaptcha/api.js?hl=fr");
+				set_fr();
+				document.getElementById("lang").setAttribute("value","fr-FR");
+				visionInfo.textContent = language.fr.visionPage
+				document.getElementById("nom").placeholder = "Nom";
+				document.getElementById("email").placeholder = "Adresse Email";
+				document.getElementById("subject").placeholder = "Sujet";
+				document.getElementById("message").placeholder = "Ecrivez-nous ici !";
+				document.getElementById("submit_btn").value = "Envoyer";
+				
+				
 		};
 		if (window.location.hash === "#EN") {
-			set_en();
-			visionInfo.textContent = language.en.visionPage;
+				document.getElementById("captcha_api").setAttribute("src","https://www.google.com/recaptcha/api.js?hl=en");
+				set_en();
+				document.getElementById("lang").setAttribute("value","en-US");
+				visionInfo.textContent = language.en.visionPage;
+				document.getElementById("nom").placeholder = "Name";
+				document.getElementById("email").placeholder = "E-mail";
+				document.getElementById("subject").placeholder = "Subject";
+				document.getElementById("message").placeholder = "Write us here !";
+				document.getElementById("submit_btn").value = "Send";
+
+			
+			
 		};
 	};
 				// RELOAD AFTER SELECTION
@@ -47,8 +67,61 @@
 		dataReload[i].onclick = function () {
 			setTimeout(function () {
 				location.reload(true);
-			}, 1000) // adjust the timer that works for you (1000 = 1 second)
+			}, 500) // adjust the timer that works for you (1000 = 1 second)
 		}
 	}
 }
 			detect_lang_change();
+
+
+						// CONTACT FORM ERRORS AND LANGUAGE PREFERENCES AND BUTTON ACTION
+
+ 		
+							var recaptcha_response = '';
+							
+							function submitUserForm() {
+								if (recaptcha_response.length == 0) {
+
+									if (document.getElementById("lang").getAttribute("value") === "en-US") {
+										document.getElementById('response-output').innerHTML = '<span style="color:red;">This field is required.</span>';
+
+									} else if (document.getElementById("lang").getAttribute("value") === "fr-FR") {
+										document.getElementById('response-output').innerHTML = '<span style="color:red;">Ce champ est requis.</span>';
+									}
+
+									return false;
+								}
+								if (document.getElementById("lang").getAttribute("value") === "en-US") {
+
+									document.getElementById("submit_btn").value = "Sent !";
+									document.getElementById("submit_btn").style.background = 'rgb(180, 227, 61)';
+
+
+									setTimeout(function () {
+										document.getElementById("submit_btn").value = "Send";
+										document.getElementById("submit_btn").style.background = '#fff'
+									}, 4000);
+								}
+								else if (document.getElementById("lang").getAttribute("value") === "fr-FR") {
+
+									document.getElementById("submit_btn").value = "Envoyé !";
+									document.getElementById("submit_btn").style.background = 'rgb(180, 227, 61)';
+
+
+									setTimeout(function () {
+										document.getElementById("submit_btn").value = "Envoyer";
+										document.getElementById("submit_btn").style.background = '#fff'
+									}, 4000);
+								}
+
+								setTimeout(function () {
+									document.getElementById("contactForm").reset();
+									grecaptcha.reset();
+								}, 2000);
+								return true;
+							}
+
+							function verifyCaptcha(token) {
+								recaptcha_response = token;
+								document.getElementById('response-output').innerHTML = '';
+							}
--- ressources/submit.php
+++ ressources/submit.php
@@ -1,5 +1,35 @@
 <?php
+// (A) PROCESS STATUS
+$error = "";
+
+// (B) VERIFY CAPTCHA
+// 6Leg6YUdAAAAANE_d0DyY35FZgz9JdDgJtp_tcOs
+$secret = "6Leg6YUdAAAAANE_d0DyY35FZgz9JdDgJtp_tcOs"; // CHANGE THIS TO YOUR OWN!
+$url = "https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=" . $_POST["g-recaptcha-response"];
+$verify = json_decode(file_get_contents($url));
+
+$mailFrom = $_POST['your-email'];
+$name = $_POST['your-name'];
+$to = "Papercut@user.com"; // ! CHANGE THIS TO YOUR OWN !
+$subject = $_POST['your-subject'];
+$body = $_POST['your-message'];
+$headers[] = "From: $name <$mailFrom>";
+
+// Mail it
+if ($verify->success) {
+  // (C) SEND MAIL
+
+  foreach ($_POST as $k => $v) {
+    if ($k != "g-recaptcha-response") {
+      $body;
+    };
+  };
 
 
-
-?>
+  if (!mail($to, $subject, $body, implode("\r\n", $headers))) {
+    $error = "Failed to send email";
+  };
+} else {
+  $error = "Invalid captcha";
+};
+?>;
